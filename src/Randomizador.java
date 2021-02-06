d9793d82f924d505e54c7d7555b4f6a7f048347a
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-02-06 13:42:45 -0300

    remove unnecessary verifications

--- src/Randomizador.java
+++ src/Randomizador.java
@@ -7,18 +7,11 @@ public class Randomizador
 
     public static Random getRandom()
     {
-        if(useShared) {
-            return rand;
-        }
-        else {
-            return new Random();
-        }
+        return RAND;
     }
     
     public static void reset()
     {
-        if(useShared) {
-            rand.setSeed(SEED);
-        }
+        RAND.setSeed(SEED);
     }
 }
