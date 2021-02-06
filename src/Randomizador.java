7881d2d2624a19df56582a334d2fc77d6fcbeecf
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-02-06 14:29:49 -0300

    remove seed property of random's argument

--- src/Randomizador.java
+++ src/Randomizador.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Randomizador
 {
     private static final int SEED = 1111;
-    private static final Random RAND = new Random(SEED);
+    private static final Random RAND = new Random();
 
     public static Random getRandom()
     {
