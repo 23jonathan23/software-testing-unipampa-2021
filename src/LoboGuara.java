535d4d28b209392694b18dd1630f11170bd343bd
Matheus Luis Machado Sonego <gp.matheusluis@gmail.com>
2021-02-09 18:01:58 -0300

    Solução para daALuz

--- src/LoboGuara.java
+++ src/LoboGuara.java
@@ -109,10 +109,8 @@ public class LoboGuara
     {
         List<Localizacao> livre = campo.localizacoesAdjacentesLivres(localizacao);
         int nascimentos = procria();
-        for(int b = 0; b < nascimentos; b++) {
-            if(livre.isEmpty()) {
-                break;
-            }
+	int n = livre.size();
+        for(int b = 0; b < n && b < nascimentos; b++) {
             Localizacao loc = livre.remove(0);
             LoboGuara jovem = new LoboGuara(false, campo, loc);
             novosLobos.add(jovem);
