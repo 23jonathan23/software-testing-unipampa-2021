335dbbf69a0b231ae3d8becfac86a69369333037
Silvio Marques <silvio.mms2016@gmail.com>
2021-02-21 20:38:24 -0300

    refactor: adjusting indentation of Contador, Campo and CampoEstatistica classes

--- src/edu/unipampa/es/rp2/marco1/CampoEstatistica.java
+++ src/edu/unipampa/es/rp2/marco1/CampoEstatistica.java
@@ -3,74 +3,87 @@ package edu.unipampa.es.rp2.marco1;
 import java.util.HashMap;
 
 public class CampoEstatistica {
-    private final HashMap<Class<?>, Contador> contadores;
-    private boolean contadoresValidos;
+  private final HashMap<Class<?>, Contador> contadores;
+  private boolean contadoresValidos;
 
-    public CampoEstatistica() {
-        contadores = new HashMap<Class<?>, Contador>();
-        contadoresValidos = true;
+  public CampoEstatistica() {
+    contadores = new HashMap<Class<?>, Contador>();
+    contadoresValidos = true;
+  }
+
+  public String getPopulationDetails(Campo campo) {
+    StringBuffer buffer = new StringBuffer();
+
+    if (!contadoresValidos) {
+      geraContadores(campo);
     }
 
-    public String getPopulationDetails(Campo campo) {
-        StringBuffer buffer = new StringBuffer();
-        if (!contadoresValidos) {
-            geraContadores(campo);
-        }
-        for (Class<?> chave : contadores.keySet()) {
-            Contador info = contadores.get(chave);
-            buffer.append(info.getName());
-            buffer.append(": ");
-            buffer.append(info.getCount());
-            buffer.append(' ');
-        }
-        return buffer.toString();
+    for (Class<?> chave : contadores.keySet()) {
+      Contador info = contadores.get(chave);
+      buffer.append(info.getName());
+      buffer.append(": ");
+      buffer.append(info.getCount());
+      buffer.append(' ');
     }
 
-    public void redefine() {
-        contadoresValidos = false;
-        for (Class<?> chave : contadores.keySet()) {
-            Contador contador = contadores.get(chave);
-            contador.reset();
-        }
+    return buffer.toString();
+  }
+
+  public void redefine() {
+    contadoresValidos = false;
+
+    for (Class<?> chave : contadores.keySet()) {
+      Contador contador = contadores.get(chave);
+      contador.reset();
+    }
+  }
+
+  public void incrementaContador(Class<?> animalClass) {
+    Contador contador = contadores.get(animalClass);
+
+    if (contador == null) {
+      contador = new Contador(animalClass.getName());
+      contadores.put(animalClass, contador);
     }
 
-    public void incrementaContador(Class<?> animalClass) {
-        Contador contador = contadores.get(animalClass);
-        if (contador == null) {
-            contador = new Contador(animalClass.getName());
-            contadores.put(animalClass, contador);
-        }
-        contador.increment();
+    contador.increment();
+  }
+
+  public void contadorFinalizado() {
+    contadoresValidos = true;
+  }
+
+  public boolean checarViabilidade(Campo campo) {
+    int nonZero = 0;
+
+    if (!contadoresValidos) {
+      geraContadores(campo);
     }
 
-    public void contadorFinalizado() {
-        contadoresValidos = true;
+    for (Class<?> key : contadores.keySet()) {
+      Contador info = contadores.get(key);
+
+      if (info.getCount() > 0) {
+        nonZero++;
+      }
     }
 
-    public boolean checarViabilidade(Campo campo) {
-        int nonZero = 0;
-        if (!contadoresValidos) {
-            geraContadores(campo);
-        }
-        for (Class<?> key : contadores.keySet()) {
-            Contador info = contadores.get(key);
-            if (info.getCount() > 0) {
-                nonZero++;
-            }
-        }
-        return nonZero > 1;
-    }
+    return nonZero > 1;
+  }
 
-    private void geraContadores(Campo campo) {
-        redefine();
-        for (int linha = 0; linha < campo.getProfundidade(); linha++) {
-            for (int coluna = 0; coluna < campo.getLargura(); coluna++) {
-                Object animal = campo.getObjectAt(linha, coluna);
-                if (animal != null) {
-                    incrementaContador(animal.getClass());
-                }
-            }
+  private void geraContadores(Campo campo) {
+    redefine();
+
+    for (int linha = 0; linha < campo.getProfundidade(); linha++) {
+      for (int coluna = 0; coluna < campo.getLargura(); coluna++) {
+        Object animal = campo.getObjectAt(linha, coluna);
+
+        if (animal != null) {
+          incrementaContador(animal.getClass());
         }
-        contadoresValidos = true;
+      }
     }
+    
+    contadoresValidos = true;
+  }
 }
--- src/edu/unipampa/es/rp2/marco1/Contador.java
+++ src/edu/unipampa/es/rp2/marco1/Contador.java
@@ -1,27 +1,27 @@
 package edu.unipampa.es.rp2.marco1;
 
 public class Contador {
-    private String name;
-    private int count;
+  private String name;
+  private int count;
 
-    public Contador(String name) {
-        this.name = name.substring(12);
-        count = 0;
-    }
+  public Contador(String name) {
+    this.name = name.substring(12);
+    count = 0;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public int getCount() {
-        return count;
-    }
+  public int getCount() {
+    return count;
+  }
 
-    public void increment() {
-        count++;
-    }
+  public void increment() {
+    count++;
+  }
 
-    public void reset() {
-        count = 0;
-    }
+  public void reset() {
+    count = 0;
+  }
 }
