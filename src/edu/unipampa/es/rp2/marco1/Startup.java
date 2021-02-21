84391b44348663595e48c83e21b1bb039c4ac43f
Silvio Marques <silvio.mms2016@gmail.com>
2021-02-21 19:40:36 -0300

    refactor: renaming some variables and methods + adjusting menu texts on startup class

--- src/edu/unipampa/es/rp2/marco1/Startup.java
+++ src/edu/unipampa/es/rp2/marco1/Startup.java
@@ -3,93 +3,81 @@ package edu.unipampa.es.rp2.marco1;
 import java.util.Scanner;
 
 public class Startup {
+  public static void main(String[] args) {
+    Scanner entradas = new Scanner(System.in);
+    Simulador simulador = new Simulador();
 
-    public static void main(String[] args) {
+    System.out.println("#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#");
+    System.out.println("   Simulador de Lobos-Guará e Ovelhas   ");
+    System.out.println("        Grupo 6 de RP II 2020.02        ");
 
-        Scanner e = new Scanner(System.in);
-        Simulador simulador = new Simulador();
+    menu(simulador, entradas);
+  }
 
-        System.out.println("------ SIMULADOR DE LOBOS-GUARA E OVELHAS ------");
-        System.out.println("---- VERSÃO REFATORADA PELO GRUPO 6 DE RP II ----");
-        menu(simulador, e);
+  public static void menu(Simulador simulador, Scanner entradas) {
+    int comando = 0;
 
+    System.out.println("#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#");
+    System.out.println("1 - Executar uma etapa");
+    System.out.println("2 - Executar uma quantidade x de etapas");
+    System.out.println("3 - Executar uma simulação automática");
+    System.out.println("4 - Reiniciar o simulador");
+    System.out.println("5 - Sair");
+    System.out.println("#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#");
+
+    String entrada = entradas.nextLine();
+
+    if (podeSerConvertidoParaInteiro(entrada)) {
+      comando = Integer.parseInt(entrada);
+
+      switch (comando) {
+        case 1:
+          simulador.simulacaoUmaEtapa();
+          menu(simulador, entradas);
+          break;
+
+        case 2:
+          System.out.println("Digite a quantidade de etapas a ser executada:");
+          String quantidadeDeEtapas = entradas.nextLine();
+
+          if (podeSerConvertidoParaInteiro(quantidadeDeEtapas)) {
+            simulador.simulacao(Integer.parseInt(quantidadeDeEtapas));
+            menu(simulador, entradas);
+          } else {
+            System.err.println("Entrada inválida.");
+            menu(simulador, entradas);
+          }
+          break;
+
+        case 3:
+          simulador.executaLongaSimulacao();
+          menu(simulador, entradas);
+          break;
+
+        case 4:
+          simulador.redefine();
+          menu(simulador, entradas);
+          break;
+
+        case 5:
+          System.exit(0);
+
+        default:
+          System.err.println("Entrada inválida.");
+          menu(simulador, entradas);
+      }
+    } else {
+      System.err.println("Entrada inválida.");
+      menu(simulador, entradas);
     }
+  }
 
-    public static void menu(Simulador simulador, Scanner e) {
-
-        int comando = 0;
-
-        System.out.println("-------------------------------------");
-        System.out.println("1 - Executa Uma Etapa");
-        System.out.println("2 - Executa um número x de etapas");
-        System.out.println("3 - Executa a simulação automática");
-        System.out.println("4 - Reinicia o simulador");
-        System.out.println("5 - Sair");
-        System.out.println("-------------------------------------");
-
-        String entrada = e.nextLine();
-
-        if (isInt(entrada)) {
-
-            comando = Integer.parseInt(entrada);
-
-            switch (comando) {
-
-                case 1:
-                    simulador.simulacaoUmaEtapa();
-                    menu(simulador, e);
-
-                    break;
-
-                case 2:
-
-                    System.out.println("Digite o número de etapas:");
-                    String numEtapas = e.nextLine();
-
-                    if (isInt(numEtapas)) {
-                        simulador.simulacao(Integer.parseInt(numEtapas));
-                        menu(simulador, e);
-                    } else {
-                        System.err.println("Entrada inválida.");
-                        menu(simulador, e);
-                    }
-
-                    break;
-
-                case 3:
-                    simulador.executaLongaSimulacao();
-                    menu(simulador, e);
-
-                    break;
-
-                case 4:
-                    simulador.redefine();
-                    menu(simulador, e);
-
-                    break;
-
-                case 5:
-                    System.exit(0);
-
-                default:
-                    System.err.println("Entrada inválida");
-                    menu(simulador, e);
-
-            }
-        } else {
-
-            System.err.println("Entrada inválida");
-            menu(simulador, e);
-        }
-
+  public static boolean podeSerConvertidoParaInteiro(String entrada) {
+    try {
+      Integer.parseInt(entrada);
+      return true;
+    } catch (Exception erro) {
+      return false;
     }
-
-    public static boolean isInt(String v) {
-        try {
-            Integer.parseInt(v);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
+  }
 }
