de22bffb1c5caaceb5fbcd42df7a08266f1add06
Silvio Marques <silvio.mms2016@gmail.com>
2021-02-21 20:21:36 -0300

    refactor: adjusting tab space and some indentation of Ovelha class

--- src/edu/unipampa/es/rp2/marco1/Ovelha.java
+++ src/edu/unipampa/es/rp2/marco1/Ovelha.java
@@ -4,92 +4,103 @@ import java.util.List;
 import java.util.Random;
 
 public class Ovelha {
-    private static final int IDADE_PROCRIACAO = 5;
-    private static final int IDADE_MAXIMA = 40;
-    private static final double PROBABILIDADE_PROCRIACAO = 0.15;
-    private static final int TAMANHO_MAXIMO_NINHADA = 4;
-    private static final Random RAND = Randomizador.getRandom();
+	private static final int IDADE_PROCRIACAO = 5;
+	private static final int IDADE_MAXIMA = 40;
+	private static final double PROBABILIDADE_PROCRIACAO = 0.15;
+	private static final int TAMANHO_MAXIMO_NINHADA = 4;
+	private static final Random RAND = Randomizador.getRandom();
 
-    private int idade;
-    private boolean vivo;
-    private Localizacao localizacao;
-    private Campo campo;
+	private int idade;
+	private boolean vivo;
+	private Localizacao localizacao;
+	private Campo campo;
 
-    public Ovelha(boolean randomAge, Campo campo, Localizacao localizacao) {
-        idade = 0;
-        vivo = true;
-        this.campo = campo;
-        setLocalizacao(localizacao);
-        if (randomAge) {
-            idade = RAND.nextInt(IDADE_MAXIMA);
-        }
-    }
+	public Ovelha(boolean randomAge, Campo campo, Localizacao localizacao) {
+		idade = 0;
+		vivo = true;
+		this.campo = campo;
 
-    public void corre(List<Ovelha> novasOvelhas) {
-        incrementaIdade();
-        if (vivo) {
-            daALuz(novasOvelhas);
-            Localizacao newLocalizacao = campo.localizacaoAdjacenteLivre(localizacao);
-            if (newLocalizacao != null) {
-                setLocalizacao(newLocalizacao);
-            } else {
-                setMorte();
-            }
-        }
-    }
+		setLocalizacao(localizacao);
 
-    public boolean estaViva() {
-        return vivo;
-    }
+		if (randomAge) {
+			idade = RAND.nextInt(IDADE_MAXIMA);
+		}
+	}
 
-    public void setMorte() {
-        vivo = false;
-        if (localizacao != null) {
-            campo.limpa(localizacao);
-            localizacao = null;
-            campo = null;
-        }
-    }
+	public void corre(List<Ovelha> novasOvelhas) {
+		incrementaIdade();
 
-    public Localizacao getLocalizacao() {
-        return localizacao;
-    }
+		if (vivo) {
+			daALuz(novasOvelhas);
+			Localizacao newLocalizacao = campo.localizacaoAdjacenteLivre(localizacao);
 
-    private void setLocalizacao(Localizacao newLocalizacao) {
-        if (localizacao != null) {
-            campo.limpa(localizacao);
-        }
-        localizacao = newLocalizacao;
-        campo.lugar(this, newLocalizacao);
-    }
+			if (newLocalizacao != null) {
+				setLocalizacao(newLocalizacao);
+			} else {
+				setMorte();
+			}
+		}
+	}
 
-    private void incrementaIdade() {
-        idade++;
-        if (idade > IDADE_MAXIMA) {
-            setMorte();
-        }
-    }
+	public boolean estaViva() {
+		return vivo;
+	}
 
-    private void daALuz(List<Ovelha> novasOvelhas) {
-        List<Localizacao> livre = campo.localizacoesAdjacentesLivres(localizacao);
-        int nascimentos = procria();
-        int n = livre.size();
-        for (int b = 0; b < n && b < nascimentos; b++) {
-            Localizacao loc = livre.remove(0);
-            Ovelha jovem = new Ovelha(false, campo, loc);
-            novasOvelhas.add(jovem);
-        }
-    }
+	public void setMorte() {
+		vivo = false;
 
-    private int procria() {
-        int nascimentos = 0;
-        if (podeProcriar() && RAND.nextDouble() <= PROBABILIDADE_PROCRIACAO) {
-            nascimentos = RAND.nextInt(TAMANHO_MAXIMO_NINHADA) + 1;
-        }
-        return nascimentos;
-    }
+		if (localizacao != null) {
+			campo.limpa(localizacao);
+			localizacao = null;
+			campo = null;
+		}
+	}
 
-    private boolean podeProcriar() {
-        return idade >= IDADE_PROCRIACAO;
-    }
+	public Localizacao getLocalizacao() {
+		return localizacao;
+	}
+
+	private void setLocalizacao(Localizacao newLocalizacao) {
+		if (localizacao != null) {
+			campo.limpa(localizacao);
+		}
+
+		localizacao = newLocalizacao;
+		
+		campo.lugar(this, newLocalizacao);
+	}
+
+	private void incrementaIdade() {
+		idade++;
+
+		if (idade > IDADE_MAXIMA) {
+			setMorte();
+		}
+	}
+
+	private void daALuz(List<Ovelha> novasOvelhas) {
+		List<Localizacao> livre = campo.localizacoesAdjacentesLivres(localizacao);
+		int nascimentos = procria();
+		int n = livre.size();
+
+		for (int b = 0; b < n && b < nascimentos; b++) {
+			Localizacao loc = livre.remove(0);
+			Ovelha jovem = new Ovelha(false, campo, loc);
+			novasOvelhas.add(jovem);
+		}
+	}
+
+	private int procria() {
+		int nascimentos = 0;
+
+		if (podeProcriar() && RAND.nextDouble() <= PROBABILIDADE_PROCRIACAO) {
+			nascimentos = RAND.nextInt(TAMANHO_MAXIMO_NINHADA) + 1;
+		}
+		
+		return nascimentos;
+	}
+
+	private boolean podeProcriar() {
+		return idade >= IDADE_PROCRIACAO;
+	}
 }
