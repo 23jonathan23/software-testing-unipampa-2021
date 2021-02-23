83da5e6cfdd5a64a10306b167e166a9b7e32b1ba
rafael <rafaelmardegan89@gmail.com>
2021-02-22 21:12:09 -0300

    Resolvendo conflitos de novos pacotes.

--- src/main/java/edu/unipampa/es/rp2/marco1/Animal.java
+++ src/main/java/edu/unipampa/es/rp2/marco1/Animal.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package edu.unipampa.es.rp2.marco1;
+package main.java.edu.unipampa.es.rp2.marco1;
 
 import java.util.Random;
 
