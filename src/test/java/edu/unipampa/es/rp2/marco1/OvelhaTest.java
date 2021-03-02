ea419de5e72b8aa71c597183b521467190325212
Matheus Luis Machado Sonego <gp.matheusluis@gmail.com>
2021-03-02 05:06:39 -0300

    Ovelha test class

---
+++ src/test/java/edu/unipampa/es/rp2/marco1/OvelhaTest.java
@@ -0,0 +1,69 @@
+
+package edu.unipampa.es.rp2.marco1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class OvelhaTest {
+    Campo campo;
+    
+    @Before
+    public void setup(){
+        campo = new Campo(50, 50);
+    }
+
+    @Test
+    public void testWhenTheSheepDiesItClearsTheCampo(){
+        Localizacao localizacao = new Localizacao(10, 10);
+        Ovelha ovelha = new Ovelha(true, campo, localizacao);
+        ovelha.setMorte();
+        assertNull(campo.getObjectAt(localizacao));
+    }
+    
+    @Test
+    public void testWhenTheSheepReachesMaxAgeItDies() {
+        Localizacao localizacao = new Localizacao(10, 10);
+        Ovelha ovelha = new Ovelha(true, campo, localizacao);
+        ovelha.idade = ovelha.IDADE_MAXIMA;
+        ovelha.incrementaIdade();
+        assertFalse(ovelha.estaViva());
+    }
+    
+    @Test
+    public void testWhenProcriaIsCalledItReturnsAValidNumber(){
+        Localizacao localizacao = new Localizacao(10, 10);
+        Ovelha ovelha = new Ovelha(true, campo, localizacao);
+        int n = ovelha.procria();
+        assertTrue(n >= 0 && n <= ovelha.TAMANHO_MAXIMO_NINHADA);
+    }
+    
+    @Test
+    public void testTheSheepGivesBirthToAsManySheepsAsPossible() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException{
+        Localizacao localizacao = new Localizacao(10, 0);
+        Ovelha ovelha = new Ovelha(true, campo, localizacao);
+        int n = ovelha.procria();
+        List<Localizacao> espacoLivre = campo.localizacoesAdjacentesLivres(localizacao);
+        List<Ovelha> arrayOvelha = new ArrayList<Ovelha>();
+        Method method = Ovelha.class.getDeclaredMethod("daALuz", List.class);
+        method.setAccessible(true);
+        try{
+            method.invoke(ovelha, arrayOvelha);
+        }catch(Exception e){};
+        
+        assertTrue(arrayOvelha.size() <= espacoLivre.size());
+    }
+    
+    @Test
+    public void testWhenTheSetLocationMethodIsCalledTheSheepChangesItsLocation(){
+        Localizacao localizacao = new Localizacao(10, 10);
+        Ovelha ovelha = new Ovelha(true, campo, localizacao);
+        Localizacao novaLocalizacao = new Localizacao(10, 11);
+        ovelha.setLocalizacao(novaLocalizacao);
+        assertNotNull(campo.getObjectAt(novaLocalizacao));
+    }
+}
