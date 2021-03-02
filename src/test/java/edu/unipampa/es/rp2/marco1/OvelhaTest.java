45d668a5ed62830a0282fe01486525c5080798ab
Jonathan Raphael <jonathanraphael74@outlook.com>
2021-03-02 20:15:09 -0300

    tests Ovelha class fixed

--- src/test/java/edu/unipampa/es/rp2/marco1/OvelhaTest.java
+++ src/test/java/edu/unipampa/es/rp2/marco1/OvelhaTest.java
@@ -1,29 +1,28 @@
 package edu.unipampa.es.rp2.marco1;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 import java.util.List;
 import java.util.ArrayList;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class OvelhaTest {
     Campo campo;
-    
-    @Before
-    public void setup(){
+
+    @BeforeEach
+    public void setup() {
         campo = new Campo(50, 50);
     }
 
     @Test
-    public void testWhenTheSheepDiesItClearsTheCampo(){
+    public void testWhenTheSheepDiesItClearsTheCampo() {
         Localizacao localizacao = new Localizacao(10, 10);
         Ovelha ovelha = new Ovelha(true, campo, localizacao);
         ovelha.setMorte();
         assertNull(campo.getObjectAt(localizacao));
     }
-    
+
     @Test
     public void testWhenTheSheepReachesMaxAgeItDies() {
         Localizacao localizacao = new Localizacao(10, 10);
@@ -32,17 +31,18 @@ public class OvelhaTest {
         ovelha.incrementaIdade();
         assertFalse(ovelha.estaViva());
     }
-    
+
     @Test
-    public void testWhenProcriaIsCalledItReturnsAValidNumber(){
+    public void testWhenProcriaIsCalledItReturnsAValidNumber() {
         Localizacao localizacao = new Localizacao(10, 10);
         Ovelha ovelha = new Ovelha(true, campo, localizacao);
         int n = ovelha.procria();
         assertTrue(n >= 0 && n <= ovelha.TAMANHO_MAXIMO_NINHADA);
     }
-    
+
     @Test
-    public void testTheSheepNeverGivesBirthToMoreThanTheSpaceAvailableInCampo() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException{
+    public void testTheSheepNeverGivesBirthToMoreThanTheSpaceAvailableInCampo()
+            throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException {
         Localizacao localizacao = new Localizacao(10, 0);
         Ovelha ovelha = new Ovelha(true, campo, localizacao);
         int n = ovelha.procria();
@@ -50,15 +50,17 @@ public class OvelhaTest {
         List<Ovelha> arrayOvelha = new ArrayList<Ovelha>();
         Method method = Ovelha.class.getDeclaredMethod("daALuz", List.class);
         method.setAccessible(true);
-        try{
+        try {
             method.invoke(ovelha, arrayOvelha);
-        }catch(Exception e){};
-        
+        } catch (Exception e) {
+        }
+        ;
+
         assertTrue(arrayOvelha.size() <= espacoLivre.size());
     }
-    
+
     @Test
-    public void testWhenTheSetLocationMethodIsCalledTheSheepChangesItsLocation(){
+    public void testWhenTheSetLocationMethodIsCalledTheSheepChangesItsLocation() {
         Localizacao localizacao = new Localizacao(10, 10);
         Ovelha ovelha = new Ovelha(true, campo, localizacao);
         Localizacao novaLocalizacao = new Localizacao(10, 11);
