6ada939c665bc23fa73b18c79df4c4a89cdd791a
rafael <rafaelmardegan89@gmail.com>
2021-03-07 15:53:26 -0300

    Criando classe de teste SimuladorTelaTest.java

---
+++ src/test/java/edu/unipampa/es/rp2/marco1/SimuladorTelaTest.java
@@ -0,0 +1,28 @@
+package edu.unipampa.es.rp2.marco1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class SimuladorTelaTest {
+     
+    SimuladorTela tela;
+    
+    @BeforeEach
+    public void setup(){
+        tela = new SimuladorTela(50, 50);
+    }
+
+    @Test
+    public void testSetCor() {
+    }
+
+    @Test
+    public void testMostraStatus() {
+    }
+
+    @Test
+    public void testChecarViabilidade() {
+    }
+    
+}
